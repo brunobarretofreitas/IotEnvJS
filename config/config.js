1844fc7b99c1e0c611b7742f1016725cd472db74
Bruno Barreto <Bruno Barreto>
2017-06-01 15:25:06 -0300

    First Commit

---
+++ config/config.js
@@ -0,0 +1,10 @@
+var config = {};
+
+config.brokerHost = "broker.mqttdashboard.com";
+config.brokerPort = "1883";
+config.brokerUser = "";
+config.brokerPassword = "";
+
+config.mongoHost = "localhost";
+config.mongoPort = "";
+config.mongoDatabase = "iotenv"; 
